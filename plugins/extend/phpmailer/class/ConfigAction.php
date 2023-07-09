80dda81c0a4e4a32f4867000a47c653ecee97410
Jirka Daněk <jirka.danek@jdanek.eu>
2023-07-09 11:26:05 +0200

    Add FoSC lib dependency, CS fix, remove unused imports

--- plugins/extend/phpmailer/class/ConfigAction.php
+++ plugins/extend/phpmailer/class/ConfigAction.php
@@ -11,7 +11,7 @@ class ConfigAction extends BaseConfigAction
 {
     protected function getFields(): array
     {
-        $langPrefix = '%p:phpmailer.cfg';
+        $langPrefix = '%p:phpmailer.config';
 
         $gen = new FieldGenerator($this->plugin);
         $gen->generateField('use_smtp', $langPrefix, '%checkbox')
@@ -29,7 +29,7 @@ class ConfigAction extends BaseConfigAction
             ->generateField('smtp_user', $langPrefix, '%text', ['class' => 'inputmedium'])
             ->generateField('smtp_pass', $langPrefix, '%password', ['class' => 'inputmedium']);
 
-        $emailSetupLink = ' <a href="' . _e(Router::admin('settings', ['fragment' => 'settings_emails'])) . '" target="_blank" class="button"><img src="' . Router::path('admin/images/icons/action.png') . '" alt="setting" class="icon">' . _lang('phpmailer.cfg.email_setting') . '</a>';
+        $emailSetupLink = ' <a href="' . _e(Router::admin('settings', ['fragment' => 'settings_emails'])) . '" target="_blank" class="button"><img src="' . Router::path('admin/public/images/icons/action.png') . '" alt="setting" class="icon">' . _lang('phpmailer.config.email_setting') . '</a>';
         $gen->generateField('sender_email', $langPrefix, '%text', ['class' => 'inputmedium', 'readonly'], 'text', ['after' => $emailSetupLink])
             ->generateField('sender_name', $langPrefix, '%text', ['class' => 'inputmedium'])
             ->generateField('smtp_auto_tls', $langPrefix, '%checkbox');
--- plugins/extend/phpmailer/lang/cs.php
+++ plugins/extend/phpmailer/lang/cs.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'cfg.use_smtp' => 'Použít SMTP ',
-    'cfg.smtp_secure' => 'SMTP Secure',
-    'cfg.smtp_auth' => 'SMTP Auth',
-    'cfg.smtp_port' => 'Port <small>(25, 465, 587)</small>',
-    'cfg.smtp_host' => 'Hostitel <small>(Server)</small>',
-    'cfg.smtp_user' => 'Uživatelské jméno',
-    'cfg.smtp_pass' => 'Heslo',
-    'cfg.sender_email' => 'Email odesílatele',
-    'cfg.email_setting' => 'Nastavit',
-    'cfg.sender_name' => 'Název odesílatele',
-    'cfg.smtp_auto_tls' => 'SMTP auto TLS',
+    'config.use_smtp' => 'Použít SMTP ',
+    'config.smtp_secure' => 'SMTP Secure',
+    'config.smtp_auth' => 'SMTP Auth',
+    'config.smtp_port' => 'Port <small>(25, 465, 587)</small>',
+    'config.smtp_host' => 'Hostitel <small>(Server)</small>',
+    'config.smtp_user' => 'Uživatelské jméno',
+    'config.smtp_pass' => 'Heslo',
+    'config.sender_email' => 'Email odesílatele',
+    'config.email_setting' => 'Nastavit',
+    'config.sender_name' => 'Název odesílatele',
+    'config.smtp_auto_tls' => 'SMTP auto TLS',
 ];
